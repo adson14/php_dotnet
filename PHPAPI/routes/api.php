d986b60761f8baf6888902316be7f40b3499d7c9
Adson Souza <ty.msn@hotmail.com>
2021-05-31 22:35:45 -0300

    Implementações de Autenticação na API 1 parte

--- PHPAPI/routes/api.php
+++ PHPAPI/routes/api.php
@@ -1,4 +1,14 @@
 <?php
 
 
-Route::apiResource('pessoas','api\PessoaController');
+
+Route::group(['middleware'=>['api', 'cors' => \App\Http\Middleware\Cors::class]],function (){
+    Route::post('auth/register', 'Auth\ApiRegisterController@register');
+});
+
+
+
+
+Route::middleware('apiJWT')->group(function() {
+    Route::apiResource('pessoas','api\PessoaController');
+});
